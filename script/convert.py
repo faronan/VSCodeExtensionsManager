a83552e699c605466daded52393cf260e4ac7a47
faronan <toshiki.ito88@gmail.com>
2021-12-10 00:59:50 +0900

    add linters

--- script/convert.py
+++ script/convert.py
@@ -1,8 +1,9 @@
-import json
 import itertools
+import json
+
 import requests
-from pytablewriter import MarkdownTableWriter
 from bs4 import BeautifulSoup
+from pytablewriter import MarkdownTableWriter
 
 
 def flatten(iter):
@@ -12,30 +13,33 @@ def flatten(iter):
 def create_soup(url):
     res = requests.get(url)
     res.raise_for_status()
-    return BeautifulSoup(res.text, 'html.parser')
+    return BeautifulSoup(res.text, "html.parser")
 
 
 def get_desc(soup):
     shortdesc = soup.select(
-        "#section-banner > div > table > tbody > tr > td.item-header > div > div.ux-item-shortdesc")[0].get_text()
+        "#section-banner > div > table > tbody > tr > td.item-header > div > ",
+        "div.ux-item-shortdesc",
+    )[0].get_text()
     return shortdesc
 
 
 def get_install_count(soup):
     install_count = soup.select(
-        "#section-banner > div > table > tbody > tr > td.item-header > div > div.ux-item-second-row-wrapper > div.ux-item-rating > span")[0].get_text()
+        "#section-banner > div > table > tbody > tr > td.item-header > div >",
+        "div.ux-item-second-row-wrapper > div.ux-item-rating > span",
+    )[0].get_text()
     return install_count
 
 
 def get_img(soup):
-    src = soup.select("#vss_2 > img")[0].get('src')
+    src = soup.select("#vss_2 > img")[0].get("src")
     markdown_img = "![Alternate text]({})".format(src)
     return markdown_img
 
 
 def create_extension_table_element(extension):
-    url = "https://marketplace.visualstudio.com/items?itemName={}".format(
-        extension)
+    url = "https://marketplace.visualstudio.com/items?itemName={}".format(extension)
     soup = create_soup(url)
     desc = get_desc(soup)
     install_count = get_install_count(soup)
@@ -44,12 +48,14 @@ def create_extension_table_element(extension):
 
 
 def main():
-    json_open = open('extensions.json', 'r')
+    json_open = open("extensions.json", "r")
     json_load = json.load(json_open)
     extension_list = flatten(json_load.values())
 
-    content = [flatten([[extension], create_extension_table_element(
-        extension)]) for extension in extension_list]
+    content = [
+        flatten([[extension], create_extension_table_element(extension)])
+        for extension in extension_list
+    ]
 
     writer = MarkdownTableWriter(
         headers=["name", "desc", "installs", "image"],
